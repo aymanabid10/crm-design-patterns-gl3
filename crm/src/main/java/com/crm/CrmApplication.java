aa67da01d17332db083bbc83194f7e92c3200194
Aymen Abid <aymen.abid2023@gmail.com>
2025-12-16 01:07:43 +0100

    fix : fixing spring boot project folder

--- crm/src/main/java/com/crm/CrmApplication.java
+++ crm/src/main/java/com/crm/CrmApplication.java
@@ -2,6 +2,7 @@ package com.crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import io.github.cdimascio.dotenv.Dotenv;
 
 /**
  * Application principale du CRM - EPIC 3: Gestion des Leads et Contacts
@@ -14,6 +15,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class CrmApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
         SpringApplication.run(CrmApplication.class, args);
         System.out.println("  CRM Leads & Contacts API Started");
         System.out.println("  Swagger UI: http://localhost:8080/swagger-ui.html");
